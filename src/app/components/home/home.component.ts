41b49be49c9fda2383d3ac3a12140ca3bade360a
chunghp3006 <chunghp3006@gmail.com>
2022-08-10 10:58:19 +0700

    update

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,50 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-
+import home from "../../../assets/data/home.json"
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-  homeBanner = [
-    {
-      option: 'Specialist examination',
-      icon: 'fa-regular fa-hospital',
-    },
-    {
-      option: 'Remote examination',
-      icon: 'fa-regular fa-mobile-screen',
-    },
-    {
-      option: 'General examination',
-      icon: 'fa-regular fa-ballot-check',
-    },
-    {
-      option: 'Medical test',
-      icon: 'fa-regular fa-microscope',
-    },
-    {
-      option: 'Mental health',
-      icon: 'fa-regular fa-brain',
-    },
-    {
-      option: 'Dental examination',
-      icon: 'fa-regular fa-tooth',
-    },
-    {
-      option: 'Surgery package',
-      icon: 'fa-regular fa-scalpel',
-    },
-    {
-      option: 'Medical products',
-      icon: 'fa-regular fa-truck-medical',
-    },
-    {
-      option: 'Business health',
-      icon: 'fa-regular fa-briefcase-medical',
-    },
-  ];
-  constructor() {}
+  jkn = home
+
+  constructor() {
+  }
 
   ngOnInit(): void {}
 }
